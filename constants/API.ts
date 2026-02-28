89c82a3dca78b2d4350a56751405c1782d87fd38
SaajidMohammed <saajidmohammed6@gmail.com>
2026-02-28 11:25:46 +0530

    Updated .env files in mobile application

--- constants/API.ts
+++ constants/API.ts
@@ -1,7 +1,7 @@
 // constants/API.ts
 
 // Replace with your actual Render URL
-export const BASE_URL = 'https://bloodlink-server-g6ee.onrender.com/api';
+export const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const ENDPOINTS = {
   LOGIN: '/auth/login',
